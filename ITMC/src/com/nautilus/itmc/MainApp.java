52faf8975b8010215c511fe277ff584d96c6317a
Binh Truong <Binh Truong>
2016-07-14 15:39:21 +0700

    Binh cap nhat Main

--- ITMC/src/com/nautilus/itmc/MainApp.java
+++ ITMC/src/com/nautilus/itmc/MainApp.java
@@ -5,7 +5,7 @@ public class MainApp {
 	public static void main(String args[]) {
 
 		InformationNetwork aNetwork = new InformationNetwork();
-		aNetwork.readdData("E:\\TruongBinh\\GocHocTap\\CaoHocKHTN\\MachineLearning\\DoAnMayHoc\\Slide\\itcm\\data\\data.txt");
+		aNetwork.readdData("E:\\TruongBinh\\GocHocTap\\CaoHocKHTN\\MachineLearning\\DoAnMayHoc\\Slide\\itcm\\data\\CreditApprovalDS.txt");
 		
 	}
 }
