97f82e047746f0515cbb480c674f1567cdb14d8e
Dao <vdao5@vdc.csc.com>
2016-07-14 15:08:30 +0700

    Chuan bi doc du lieu len

--- ITMC/src/com/nautilus/itmc/ValueItem.java
+++ ITMC/src/com/nautilus/itmc/ValueItem.java
@@ -2,6 +2,7 @@ package com.nautilus.itmc;
 
 public class ValueItem {
 	private boolean nominal;
+	private boolean missing;
 	private String dValue;
 	private double rValue;
 	
@@ -9,12 +10,36 @@ public class ValueItem {
 		return rValue;
 	}
 	
+	public void setRValue(double rv) {
+		rValue = rv;
+	}
+	
 	public String getDValue() {
 		return dValue;
+	}
+	
+	public void setDValue(String dv) {
+		dValue = dv;
 	}
 	
 	@Override
 	public String toString() {
 		return nominal?dValue:(""+rValue);
+	}
+
+	public boolean isNominal() {
+		return nominal;
+	}
+
+	public void setNominal(boolean nominal) {
+		this.nominal = nominal;
+	}
+
+	public boolean isMissing() {
+		return missing;
+	}
+
+	public void setMissing(boolean missing) {
+		this.missing = missing;
 	}
 }
